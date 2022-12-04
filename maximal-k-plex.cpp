a8803ccf24e3e8d342ea1206eeecfd344586d5f0
Debdeep Naha <49310335+Debdeep23@users.noreply.github.com>
2022-12-04 12:40:30 +0530

    Update maximal-k-plex.cpp

--- maximal-k-plex.cpp
+++ maximal-k-plex.cpp
@@ -1,43 +1,12 @@
 #include<stdio.h>
-
+#include<bits/stdc++.h>
 #include<iostream>
+#include<chrono>
 #include "graph.h"
 #include "bkplex.h"
 using namespace std;
+using namespace std::chrono;
 
-
-/*void input(graph &g){
-    int nn , mm;
-    cin >> nn >> mm;
-    g.n = nn;
-    g.m = mm;
-    set<int>gg = g.vertex_set;
-    unordered_map<int, vector<int>> adj1 = g.adj;
-    unordered_map<int,int> deg = g.vertex_degree;
-    unordered_set<long long int> adj_mat1 = g.adj_mat;
-    int mx_degree = 0;
-    for(int i =0 ; i<mm ; i++) {
-        int u , v;
-        cin >> u >> v;
-        gg.insert(u);
-        gg.insert(v);
-        adj1[u].push_back(v);
-        adj1[v].push_back(u);
-        mx_degree = max(mx_degree , (int)adj1[u].size());
-        mx_degree = max(mx_degree , (int)adj1[v].size());
-        deg[u] = adj1[u].size();
-        deg[v] = adj1[v].size();
-        adj_mat1.insert(g.hash(u,v));
-        adj_mat1.insert(g.hash(v,u));
-
-
-
-    }
-
-    g.max_degree = mx_degree;
-
-
-}*/
 
 int main(int argc , char **argv) {
 
@@ -47,11 +16,23 @@ int main(int argc , char **argv) {
     //value of k we are reading ?
     //cout<<argv[1]<<endl;
 	printf("Reading graph completed\n");
+    //k = 2 , k = 3 try
 
 	bkplex algo(g, 2 , 2);
 	
 	printf("constructor called\n");
-
+    auto start = high_resolution_clock::now();
 	algo.run();
+    auto end = high_resolution_clock::now();
+
+
+    double time_taken =
+            chrono::duration_cast<chrono::nanoseconds>(end - start).count();
+
+    time_taken *= 1e-9;
+
+    cout << "Time taken by program is : " << fixed
+         << time_taken << setprecision(9);
+    cout << " sec" << endl;
 	printf("number of max near cliques: %ld\n", algo.countplex());
 }
